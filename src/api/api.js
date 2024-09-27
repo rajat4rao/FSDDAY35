9b5d91448cc360dd6221c62bcaa82c4ffac0a1ad
rajat4rao <balprao@gmail.com>
2024-09-27 19:02:30 +0530

    backend url change

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const api = {
-  apiUrl: "http://localhost:5000/api",
-  //apiUrl: "https://fsdday35-backend.onrender.com/api",
+  //apiUrl: "http://localhost:5000/api",
+  apiUrl: "https://fsdday35-backend.onrender.com/api",
 };
 
 export default api;
